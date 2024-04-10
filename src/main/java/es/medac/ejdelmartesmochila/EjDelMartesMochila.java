a6159997bf02cb93c472d3cf600a0cab6298b2d4
mdc-ara <149468225+mdc-ara@users.noreply.github.com>
2024-04-10 13:24:15 +0200

    commit inicial

---
+++ src/main/java/es/medac/ejdelmartesmochila/EjDelMartesMochila.java
@@ -0,0 +1,13 @@
+
+package es.medac.ejdelmartesmochila;
+
+/**
+ *
+ * @author asier.ruiz
+ */
+public class EjDelMartesMochila {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
