2e2b68b5ea9856fa490f7328967fdd4ef7085fe8
mdc-ara <149468225+mdc-ara@users.noreply.github.com>
2024-04-10 13:59:03 +0200

    seleccionarSouvenirs a medio implementar

--- src/main/java/es/medac/ejdelmartesmochila/EjDelMartesMochila.java
+++ src/main/java/es/medac/ejdelmartesmochila/EjDelMartesMochila.java
@@ -14,5 +14,26 @@ public class EjDelMartesMochila {
         MochilaFestival mochila = new MochilaFestival(11);
         
         List<Souvenir> souvenirs = new ArrayList<>(); // Souvenirs ofrecidos por el festival
+        List<Souvenir> sSeleccionados; // Souvenirs seleccionados
+        
+        int p, ve, pesoT, veT;
+        
+        // Souvenirs con KG y €
+        souvenirs.add(new Souvenir(1,1));
+        souvenirs.add(new Souvenir(3,8));
+        souvenirs.add(new Souvenir(5,18));
+        souvenirs.add(new Souvenir(6,22));
+        souvenirs.add(new Souvenir(7,28));
+        
+        sSeleccionados = mochila.seleccionarSouvernirs(souvenirs, 11);
+        
+        System.out.println("Souvenirs seleccionados:");
+        pesoT=veT=0;
+        for(Souvenir s:sSeleccionados){
+            pesoT+=p=s.getPeso();
+            veT+=ve=s.getValorEc();
+            System.out.println("Peso: " + p + " valor económico: " + ve );            
+        }
+        System.out.println("Peso Total: " + pesoT + " valor económico Total: " + veT);
     }
 }
--- src/main/java/es/medac/ejdelmartesmochila/MochilaFestival.java
+++ src/main/java/es/medac/ejdelmartesmochila/MochilaFestival.java
@@ -19,6 +19,25 @@ public class MochilaFestival extends Mochila implements Seleccionable {
 
     @Override
     public List<Souvenir> seleccionarSouvernirs(List<Souvenir> s, int pM) {
+        int N=s.size();
+        // Máximos € que se pueden obtener con i souvenires y w peso máximo
+        int[][]dp=new int[N+1][pM+1];
+        int peSouv;
+        int veSouv;
+        
+        for(int i=0;i<=N;i++){
+            for(int w=0;w<=pM;w++){
+                if(i==0||w==0){ // Primera fila o columna
+                    dp[i][0]=0; // Casos base: sin items o peso 0
+                    continue;
+                }
+                peSouv=s.get(i-1).getPeso();
+                veSouv=s.get(i-1).getValorEc();
+                if(peSouv>w){ // Si el peso del souvenir es mayor de lo que indica la columna
+                    dp[i][w]=dp[i-1][w]; // Se deja los €s que estaban en la fila anterior
+                }
+            }
+        }
         return(s);
     }
     
